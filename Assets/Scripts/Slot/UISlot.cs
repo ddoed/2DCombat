ad344e4b5b0aa069e8a29389ce27e38213b041ae
yimnabin <yimnabin20051022@gmail.com>
2025-02-20 09:07:55 +0900

    (20250219) skill slot, mouse drag

---
+++ Assets/Scripts/Slot/UISlot.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISlot : MonoBehaviour
+{
+    public Image itemImage;
+    Storage storage;
+    MouseDrag mouseDrag;
+
+    public void SetupStorage(Storage storage)
+    {
+        this.storage = storage;
+    }
+
+    public Storage GetStorage()
+    {
+        return storage;
+    }
+
+    public void UpdateUI(Item item)
+    {
+        if (item == null)
+        {
+            itemImage.sprite = null;
+        }
+       itemImage.sprite = item.IconImage;
+    }
+
+    public void SetupMouseDrag(Storage storage)
+    {
+        if (mouseDrag == null)
+        {
+            mouseDrag = gameObject.AddComponent<MouseDrag>();
+        }
+        else
+        {
+            mouseDrag = gameObject.GetComponent<MouseDrag>();
+        }
+        mouseDrag.SetupStorage(storage, this);
+    }
+
+    public void LockMouseDrag()
+    {
+        mouseDrag.SetDragAbility(false);
+    }
+}
